d5d21529a717849d28963d33d3794a006b333d99
Felipe <fepolato@hotmail.com>
2021-09-08 18:33:51 -0300

    router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import express, { Express } from "express";
 import cors from "cors";
-import { AddressInfo } from "net";
 
-const app: Express = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
-app.use('/', route);
+app.use("/", route);
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
-    const address = server.address() as AddressInfo;
+    const address = server.address();
     console.log(`Server is running in http://localhost: ${address.port}`);
   } else {
     console.error(`Failure upon starting server.`);
