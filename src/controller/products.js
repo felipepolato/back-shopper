dcab5b7ce1a5fa436d7d5420ddfe850a90c1a611
Felipe Polato <fepolato@hotmail.com>
2021-09-14 13:45:55 -0300

    erro para estoque inexistente

--- src/controller/products.js
+++ src/controller/products.js
@@ -37,37 +37,42 @@ module.exports.postProduct = async (req, res, next) => {
   }
 };
 
-
 ///PUT  /products/
 module.exports.putProduct = async (req, res, next) => {
   try {
     const { id, qty } = req.body;
     if (!id || !qty) {
-        throw new Error("Parâmetros Inexistentes!");
+      console.log("ERROO")
+      throw new Error("Parâmetros Inexistentes!");
     }
-    
+
     const product = await Product.findOne({
-        id,
+      id,
     });
-    
+
+    const hasStock = product.qty_stock - qty >= 0;
+    if (!hasStock) {
+      throw new Error("Estoque Indisponível!");
+    }
+
     const update = {
-        qty_stock: product.qty_stock - qty,
+      qty_stock: product.qty_stock - qty,
     };
 
-    
     const updated = await Product.findOneAndUpdate(
-        {
-            id: id,
-        },
-        update,
-        {
-            new: true,
-        }
-        );
-        console.log( updated)
-        return res.json(updated);
-        ///TODO Tratar Erro ////////
-    } catch (error) {
-    return res.json({ error });
+      {
+        id: id,
+      },
+      update,
+      {
+        new: true,
+      }
+    );
+    console.log(updated);
+    return res.json(updated);
+    ///TODO Tratar Erro ////////
+  } catch (error) {
+    console.log("ERRO AQQUI", error.message)
+    return res.status(400).json({ message:  error.message });
   }
 };
